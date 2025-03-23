a210503282d82254332819046bd1dd20f3d5d649
GurgenAbrahamyan <gurgenabrahamyan22@gmail.com>
2025-03-23 12:02:28 +0400

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.backtrack_quiz"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.backtrack_quiz"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
